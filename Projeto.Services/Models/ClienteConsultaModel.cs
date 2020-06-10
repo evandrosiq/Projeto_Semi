1cf7b5210bcd37b529ce3629b233c5c6c90f0857
evandrosiq <evandrobio@hotmail.com>
2020-06-09 22:39:55 -0300

    Projeto Semi - COTI

---
+++ Projeto.Services/Models/ClienteConsultaModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto.Services.Models
+{
+    public class ClienteConsultaModel
+    {
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
---
+++ Projeto.Services/Models/ClienteEdicaoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Services.Models
+{
+    public class ClienteEdicaoModel
+    {
+        [Required(ErrorMessage = "Informe o id do cliente.")]
+        public int IdCliente { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do cliente.")]
+        public string Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Informe um email válido.")]
+        [Required(ErrorMessage = "Informe o email do cliente.")]
+        public string Email { get; set; }
+    }
+}
